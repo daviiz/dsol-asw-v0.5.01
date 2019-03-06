403c790c2405f25ba1684f4336ac20723c07a267
WZ.Dai <daviiz1117@163.com>
2019-03-06 11:24:17 +0800

    build modeling arch

--- src/main/java/combatSysModel/OM/OM_Controller.java
+++ src/main/java/combatSysModel/OM/OM_Controller.java
@@ -1,12 +1,12 @@
 package combatSysModel.OM;
 
-import combatSysModel.portType.scen_info;
+import combatSysModel.portType.env_info;
 import combatSysModel.portType.target_info;
 import combatSysModel.portType.threat_info;
 
 public class OM_Controller extends ObjectModelBase{
 
-    public target_info identification(threat_info threat, scen_info info){
+    public target_info identification(threat_info threat, env_info info){
         return null;
     }
 
