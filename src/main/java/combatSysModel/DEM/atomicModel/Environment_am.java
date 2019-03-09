1728eca7b2c2ac8e3a5fd0d4037e75c6b2df683d
WZ.Dai <daviiz1117@163.com>
2019-03-09 11:49:44 +0800

    build modeling arch

--- src/main/java/combatSysModel/DEM/atomicModel/Environment_am.java
+++ src/main/java/combatSysModel/DEM/atomicModel/Environment_am.java
@@ -2,14 +2,23 @@ package combatSysModel.DEM.atomicModel;
 
 import combatSysModel.DEM.AtomicModelBase;
 import combatSysModel.OM.Environment_om;
+import combatSysModel.portType.env_info;
+import combatSysModel.portType.scen_info;
 import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.CoupledModel;
+import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.InputPort;
+import nl.tudelft.simulation.dsol.formalisms.devs.ESDEVS.OutputPort;
+import nl.tudelft.simulation.dsol.simtime.SimTimeDouble;
 import nl.tudelft.simulation.dsol.simulators.DEVSSimulator;
 
 public class Environment_am extends AtomicModelBase<Environment_om>{
 
+    public InputPort<Double,Double, SimTimeDouble, scen_info> in_scen_info;
+    public OutputPort<Double,Double, SimTimeDouble, env_info> out_env_info;
+
     @Override
     protected void constructPort() {
-
+        in_scen_info = new InputPort<Double, Double, SimTimeDouble, scen_info>(this);
+        out_env_info = new OutputPort<Double, Double, SimTimeDouble, env_info>(this);
     }
 
     @Override
