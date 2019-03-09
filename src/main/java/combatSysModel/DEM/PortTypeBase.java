954505236c5b657db990f09d5871ec0d1a1318c2
WZ.Dai <daviiz1117@163.com>
2019-03-09 13:31:43 +0800

    build modeling arch

---
+++ src/main/java/combatSysModel/DEM/PortTypeBase.java
@@ -0,0 +1,14 @@
+package combatSysModel.DEM;
+
+public class PortTypeBase implements java.io.Serializable {
+
+    private String senderId;
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+}
